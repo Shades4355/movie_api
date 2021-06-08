63f43d6e2580976fe175aef91f78528cd60129a7
Shades <51397439+Shades4355@users.noreply.github.com>
2021-06-08 15:16:56 -0400

    Front End started (#2)
    +templated display page
    +mapped movies array
    +created MovieTile.js
    +created loading page

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,7 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+import MovieTile from "./MovieTile"
 
 export const App = (props) => {
-  return (<h1>Make It So React</h1>)
+  const [movies, setMovies] = useState(null)
+
+  // TODO: fetch movie data from backend
+
+  if (movies) {
+    movieTile = movies.map(movie => {
+      return(
+        <MovieTile
+          name={movie.title}
+          upvotes={movie.upvotes}
+          downvotes={movie.downvotes}
+        />
+      )
+    })
+
+    return (
+      <div className='grid-container'>
+        <div className='grid-x grid-margin-x'>
+          <h1 className='small-12'>
+            Welcome to Shades' Movie Search
+          </h1>
+
+          <div className='small-4'>
+          Movie Title
+          </div>
+          <div className='small-4'>
+          Thumps Up
+          </div>
+          <div className='small-4'>
+          Thumbs Down
+          </div>
+
+          <div className='small-12'>
+            {movieTile}
+          </div>
+
+        </div>
+      </div>
+    )
+  } else {
+    return (
+      <h3>
+        Loading . . .
+      </h3>
+    )
+  }
 }
 
 export default App
