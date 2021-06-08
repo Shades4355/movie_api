a91496ebc26b63bd429c0e2c71ad104d93da7ea4
Shades <51397439+Shades4355@users.noreply.github.com>
2021-06-08 19:49:35 -0400

    Add buttons (#5)
    * Add documentation
    
    * added buttons with mouse change

--- app/javascript/react/components/MovieTile.js
+++ app/javascript/react/components/MovieTile.js
@@ -2,16 +2,42 @@ import React from 'react'
 
 export const MovieTile = (props) => {
 
+  const increment = props => { // TODO: make this increment the correct value
+    console.log("click")
+    console.log("Props: ", props)
+  }
+
+  const reveal = () => {
+    console.log(props.name, "clicked")
+  }
+
   return(
     <div className='grid-x small-12'>
-      <div className='small-4'>
+      <div className='small-4 cursor-pointer'
+           onClick={reveal}>
         {props.name}
       </div>
-      <div className='small-4'>
+      <div className='small-1'>
         {props.upvotes}
       </div>
-      <div className='small-4'>
+      <div className='small-1 cursor-pointer'
+           onClick={increment}
+      >
+        [x]
+      </div>
+      <div className='small-2'>
+      </div>
+      <div className='small-1'>
         {props.downvotes}
+      </div>
+      <div className='small-1 cursor-pointer'
+           onClick={increment}
+      >
+        [x]
+      </div>
+      <div className='small-2'>
+      </div>
+      <div >
       </div>
     </div>
   )
