8cf124eacbc3f9806e91c448fc6fba2e59396939
Shades <51397439+Shades4355@users.noreply.github.com>
2021-06-25 17:18:26 -0400

    Fix patch request (#12)
    *  buttons work
    
    * movies not displaying correctly

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,5 @@
 class Movie < ApplicationRecord
   validates :display_title, presence: true
   validates :headline, presence: true
-  validates :publication_date, presence: true, numericality: true
-  validates :upvotes, presence: true, numericality: true
-  validates :downvotes, presence: true, numericality: true
+  validates :publication_date, presence: true
 end
