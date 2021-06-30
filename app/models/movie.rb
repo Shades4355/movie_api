b9e51ad153cd57dc09c50058777cfa5ec7c3e6cf
Shades <shades4355@gmail.com>
2021-06-30 18:58:51 -0400

    rspec model: movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,6 @@ class Movie < ApplicationRecord
   validates :display_title, presence: true
   validates :headline, presence: true
   validates :publication_date, presence: true
+  validates :upvotes, numericality: true
+  validates :downvotes, numericality: true
 end
